82449ad60b6d4f55c674573afa4be22776904274
Belen Furuken <belu.furu@gmail.com>
2023-04-17 00:42:49 -0300

    correcion de la primer entrega

--- precio_final.js
+++ precio_final.js
@@ -8,21 +8,21 @@ function INGRESO_DATOS(){
     }while(neto<=0);
     console.log("El valor neto es: " + neto);
     do{
-        iva = prompt("Ingrese la alicuota");
-    }while(iva<0);
+        iva = prompt("Indeque que alicuota de IVA contiene: \n 1) 10.5% \n 2) 21% \n 3) 27%");
+    }while(!(iva<0 || iva==1 || iva==2 || iva==3));
     console.log("La alicuota es: " + iva);
     do{
         descuento = prompt("Ingrese si cuenta con algun descuento");
-    }while(descuento<0);
+    }while(descuento<0 || descuento>=100);
     console.log("Tiene un descuento de: " + descuento); 
 }
 INGRESO_DATOS();
 if(descuento == 0){
-    total = parseInt(neto)-parseInt(neto)*(parseInt(iva)/100);
+    total = parseInt(neto) + parseInt(neto)*(parseInt(iva)/100);
     alert("El precio final es de: " + total);
 }else{
-    total = parseInt(neto)-parseInt(neto)*(parseInt(iva)/100)- parseInt(descuento);
-    alert("El precio final es de: " + total);
+    total = (parseInt(neto)- parseInt(descuento)) + parseInt(neto)*(parseInt(iva)/100);
+    alert("El precio  final es de: " + total);
 }
 
 
