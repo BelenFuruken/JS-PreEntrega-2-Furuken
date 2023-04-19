2678af0938f98ac2d77e22eb71b1456cb052d227
Belen Furuken <belu.furu@gmail.com>
2023-04-18 23:05:27 -0300

    Actualizacion para la entrega 2

--- precio_final.js
+++ precio_final.js
@@ -1,4 +1,4 @@
-let neto = 0;
+/*let neto = 0;
 let iva = 0;
 let descuento = 0;
 let total = 0;
@@ -23,6 +23,45 @@ if(descuento == 0){
 }else{
     total = (parseInt(neto)- parseInt(descuento)) + parseInt(neto)*(parseInt(iva)/100);
     alert("El precio  final es de: " + total);
+}
+*/
+
+class ingresos{
+    constructor(nombre, neto){
+        this.nombre = nombre.toUpperCase();
+        this.pNeto = neto;
+        this.pFinal = parseInt(neto) + parseInt(neto) * 0.21;
+    }
+}
+let totalIngresos =[];
+let respuesta = "SI";
+do {
+    let nombre = prompt("Nombre del producto");
+    let neto = prompt("Precio del producto");
+    const nuevoIngreso = new ingresos(nombre,neto);
+    totalIngresos.push(nuevoIngreso);
+    respuesta = prompt("Quiere seguir cargando? SI/NO");
+}while(respuesta.toUpperCase() =="SI");
+//LA RESOUESTA QUEDO EN "NO"
+console.log("Se ingresaron " +totalIngresos.length+ " productos.");
+respuesta = prompt("Quiere ver los datos de los productos ingresados? SI/NO");
+if (respuesta.toUpperCase() == "SI"){
+    for(const nuevoIngreso of totalIngresos){
+        console.log(nuevoIngreso);
+    }
+}
+respuesta = "NO";
+respuesta = prompt("Desea buscar algún producto ingresado? SI/NO");
+if( respuesta.toUpperCase() =="SI"){
+    let productoBuscado = prompt("Qué producto quiere buscar?");
+    // console.log(productoBuscado);
+    const resultado = totalIngresos.find((tt) => tt.nombre.includes(productoBuscado.toUpperCase()));
+    //console.log(resultado);
+    if (resultado){
+        console.log("Producto: " + resultado.nombre + "\nPrecio neto: $" + resultado.pNeto + "\nPrecio final (con IVA 21%): $" + resultado.pFinal);
+    }else{
+        console.log("No esta ingresado ese producto");
+    }
 }
 
 
